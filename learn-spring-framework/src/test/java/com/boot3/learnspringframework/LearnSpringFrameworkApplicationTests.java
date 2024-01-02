e53474b0d503aba6417bf55623d5bbd7a17f074a
JH <JH@DESKTOP-0VB8GCM>
2024-01-02 17:11:05 +0900

    init springboot3 20240102

---
+++ learn-spring-framework/src/test/java/com/boot3/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot3.learnspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
