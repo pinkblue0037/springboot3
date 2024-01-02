c24c58d5af93165c852ef72b259565f8a71fa1fe
JH <JH@DESKTOP-0VB8GCM>
2024-01-02 17:25:27 +0900

    bean 추가

--- learn-spring-framework/src/main/java/com/boot3/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/boot3/learnspringframework/HelloWorldConfiguration.java
@@ -1,8 +1,14 @@
 package com.boot3.learnspringframework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfiguration {
-
+	
+		@Bean
+		public String name() {
+			return "init";
+			
+		}
 }
