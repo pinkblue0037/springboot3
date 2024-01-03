c9deb42d027a7942852897b900abd86357fe7cf7
JH <JH@DESKTOP-0VB8GCM>
2024-01-03 11:47:28 +0900

    수업시작 세팅.

---
+++ learn-spring-framework/src/main/java/com/boot3/learnspringgramework/game/GamingAppLauncherApplication.java
@@ -0,0 +1,21 @@
+package com.boot3.learnspringgramework.game;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.boot3.learnspringgramework.game")
+public class GamingAppLauncherApplication {
+
+	public static void main(String[] args) {
+
+		try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
+
+			context.getBean(GamingConsole.class).up();
+
+			context.getBean(GameRunner.class).run();
+
+		}
+	}
+}
---
+++ learn-spring-framework/src/main/java/com/boot3/learnspringgramework/game/PacmanGame.java
@@ -0,0 +1,19 @@
+package com.boot3.learnspringgramework.game;
+
+public class PacmanGame implements GamingConsole{
+	public void up() {
+		System.out.println("up");
+	}
+
+	public void down() {
+		System.out.println("down");
+	}
+	
+	public void left() {
+		System.out.println("left");
+	}
+
+	public void right() {
+		System.out.println("right");
+	}
+}
